57a00ee0baad90d3592fdc1d8ae47431b563e031
Ahmed <alshukairi1234@gmail.com>
2026-05-19 10:18:09 +0400

    finished case 0,1,11

--- HospitalMangament/Program.cs
+++ HospitalMangament/Program.cs
@@ -218,7 +218,7 @@ class Program
                 Console.WriteLine("Press Enter to continue...");
                 Console.ReadKey();
             }
-        }
+    }
     }
 
 
--- HotelManagment/Program.cs
+++ HotelManagment/Program.cs
@@ -20,32 +20,114 @@ class Program
         int loyalityPoints = 0;
         bool isGuestReg = false;
         bool isCheckedIn = false;
-        
+        int options;
         bool exit = false;
+        
 
-        Console.WriteLine("(0) . Register New Guest ");
-        Console.WriteLine("(1) . View Guest Information ");
-        Console.WriteLine("(2) . Check-In Guest ");
-        Console.WriteLine("(3) . Check-Out & Bill ");
-        Console.WriteLine("(4) . Apply Discount  ");
-        Console.WriteLine("(5) . Upgrade Room  ");
-        Console.WriteLine("(6) . Add Room Service Note ");
-        Console.WriteLine("(7) . Search Guest by Name ");
-        Console.WriteLine("(8) . Calculate Loyalty Points  ");
-        Console.WriteLine("(9) . Print Receipt ");
-        Console.WriteLine("(10) . Edit Guest Name  ");
-        Console.WriteLine("(11) . Exit  ");
-        Console.Write("Enter Your Choice: ");
-        int options = Convert.ToInt32(Console.ReadLine());
-
-        while (exit = false)
+        while (exit == false)
         {
+            Console.WriteLine("(0) . Register New Guest ");
+            Console.WriteLine("(1) . View Guest Information ");
+            Console.WriteLine("(2) . Check-In Guest ");
+            Console.WriteLine("(3) . Check-Out & Bill ");
+            Console.WriteLine("(4) . Apply Discount  ");
+            Console.WriteLine("(5) . Upgrade Room  ");
+            Console.WriteLine("(6) . Add Room Service Note ");
+            Console.WriteLine("(7) . Search Guest by Name ");
+            Console.WriteLine("(8) . Calculate Loyalty Points  ");
+            Console.WriteLine("(9) . Print Receipt ");
+            Console.WriteLine("(10) . Edit Guest Name  ");
+            Console.WriteLine("(11) . Exit  ");
+            Console.WriteLine("");
+            Console.Write("Enter Your Choice: ");
+            options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
-                
+                case 0:
+                    if (isGuestReg == true)
+                    {
+                        Console.WriteLine("guest already have been registered");
+                    }
+                    else
+                    {
+                        Console.Write("Enter guest's name: ");
+                        gName = Console.ReadLine();
+                        Console.Write("Enter guest's phone number: ");
+                        gPhone = Console.ReadLine();
+                        Console.Write("Choose Room Type: ");
+                        rType = Console.ReadLine();
+                        Console.Write("Nightly rate: ");
+                        nightRate = Convert.ToDouble(Console.ReadLine());
+                        rNum = Random.Shared.Next(1, 505);
+                        Console.WriteLine("Choose a check in date (DD/MM/YYYY): ");
+                        ChekInD = DateTime.Parse(Console.ReadLine());
+                        Console.WriteLine("Choose a check Out date (DD/MM/YYYY): ");
+                        CheckOutD = DateTime.Parse(Console.ReadLine());
+                        Console.Write("Enter Room Notes: ");
+                        rNotes = Console.ReadLine();
+                        Console.Write("Enter how many nights guest is staying: ");
+                        nightNum = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Discount: ");
+                        discount =float.Parse(Console.ReadLine());
+                        isGuestReg = true;
+                    }
+                    
+                    break;
+                case 1:
+                    if (isGuestReg == false)
+                    {
+                        Console.WriteLine("No Guests Registered ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("---- Guest Details ----");
+                        Console.WriteLine("");
+                        Console.WriteLine("Guest name: " + gName.ToUpper());
+                        Console.WriteLine("Guest Phone number: " + gPhone);
+                        Console.WriteLine("Room Type: " + rType);
+                        Console.WriteLine("Room Number: " + Convert.ToString(rNum));
+                        Console.WriteLine("Room Notes: " + rNotes);
+                        Console.WriteLine("Check In Date: " + Convert.ToString(ChekInD));
+                        Console.WriteLine("Check Out Date: " + Convert.ToString(CheckOutD));
+                        Console.WriteLine("Nightly Rates: " + Convert.ToString(Math.Round(nightRate)));
+                        Console.WriteLine("Nights Staying: " + Convert.ToString(nightNum));
+                        Console.WriteLine("Discount: " + Convert.ToString(discount));
+                    }
+                  
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11: //Exit
+                    Console.WriteLine("Exiting...");
+                    exit = true;
+                    break;
             }
-        }
 
+            
+            Console.WriteLine("press any key to continue..");
+            Console.ReadKey();
+            Console.Clear();
+
+            
+           
+        }
+       
 
     }
 }
